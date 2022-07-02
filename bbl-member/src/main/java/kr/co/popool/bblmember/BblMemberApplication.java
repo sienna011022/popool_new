bf0b55167dd7e05fde4d59a5e90b3d42eac7891c
홍혁준 <jjj8337721@naver.com>
2022-07-02 17:00:01 +0900

    feature/1

---
+++ bbl-member/src/main/java/kr/co/popool/bblmember/BblMemberApplication.java
@@ -0,0 +1,13 @@
+package kr.co.popool.bblmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BblMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BblMemberApplication.class, args);
+    }
+
+}
