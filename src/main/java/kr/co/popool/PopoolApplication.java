725b7dc427556dbc6624fbf81e3fa4392df069c0
홍혁준 <jjj8337721@naver.com>
2022-07-02 16:22:39 +0900

    first commit

---
+++ src/main/java/kr/co/popool/PopoolApplication.java
@@ -0,0 +1,13 @@
+package kr.co.popool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/popool/PopoolApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.popool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
