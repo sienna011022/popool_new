1bd103485000eab70a9221d14a60d5dab9620f09
sienna <20200803@sungshin.ac.kr>
2022-07-08 15:21:13 +0900

    feature/12

---
+++ bbl-career/src/test/java/kr/co/popool/BblCareerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.popool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BblCareerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
