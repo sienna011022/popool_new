2c699de777a889a0f7a4a90e34a330efed37922c
sienna <20200803@sungshin.ac.kr>
2022-07-09 22:11:43 +0900

    feature/13

--- bbl-career/src/main/java/kr/co/popool/domain/dto/CareerDto.java
+++ bbl-career/src/main/java/kr/co/popool/domain/dto/CareerDto.java
@@ -18,6 +18,10 @@ public  class CareerDto {
     @NoArgsConstructor
     public static class CAREERINFO {
 
+        @ApiModelProperty(example = "member아이디")
+        @NotBlank(message = "평가를 원하는 member 아이디를 입력하세요")
+        private String identity;
+
         @ApiModelProperty(example = "평가등급")
         @Enumerated(EnumType.STRING)
         private ScoreGrade grade;
