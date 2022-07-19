938a0177d868be039c5306921e81827483fc910e
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2022-07-20 01:41:24 +0900

    Init: 관련 dependencies가 추가된 bll-eureka 모듈 생성 #67

---
+++ bbl-eureka/src/main/java/kr/co/popool/bbleureka/BblEurekaApplication.java
@@ -0,0 +1,13 @@
+package kr.co.popool.bbleureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BblEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BblEurekaApplication.class, args);
+    }
+
+}
