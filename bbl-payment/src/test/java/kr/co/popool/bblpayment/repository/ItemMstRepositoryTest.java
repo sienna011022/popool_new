6f37e60487904c0be50e99c81d3d1a1b77bd4ea9
sochong <hsju0202@gmail.com>
2022-07-20 00:33:19 +0900

    feat: 상품 repository 설계

--- bbl-payment/src/test/java/kr/co/popool/bblpayment/repository/ItemMstRepositoryTest.java
+++ bbl-payment/src/test/java/kr/co/popool/bblpayment/repository/ItemMstRepositoryTest.java
@@ -2,26 +2,35 @@ package kr.co.popool.bblpayment.repository;
 
 import kr.co.popool.bblpayment.domain.entity.CouponEntity;
 import kr.co.popool.bblpayment.domain.entity.ItemMstEntity;
+import kr.co.popool.bblpayment.domain.entity.PeriodCouponEntity;
+import kr.co.popool.bblpayment.domain.entity.SubscribeEntity;
+import kr.co.popool.bblpayment.domain.shared.enums.CouponPeriod;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+//@DataJpaTest
+@SpringBootTest
 class ItemMstRepositoryTest {
 
     @Autowired ItemMstRepository itemMstRepository;
 
     @Test
     void save() {
-        ItemMstEntity coupon = new CouponEntity(1000, "ten",10);
-        itemMstRepository.save(coupon);
+        ItemMstEntity coupon = new CouponEntity(1000, "coupon",10);
+        ItemMstEntity periodCoupon = new PeriodCouponEntity(10000, "period-coupon", "1개월");
+        ItemMstEntity subscribe = new SubscribeEntity(3000, "subscribe", LocalDate.now());
 
-        Optional<ItemMstEntity> findItem = itemMstRepository.findById(1L);
-        System.out.println("findItem.get() = " + findItem.get());
+        itemMstRepository.save(coupon);
+        itemMstRepository.save(periodCoupon);
+        itemMstRepository.save(subscribe);
     }
 
 }
