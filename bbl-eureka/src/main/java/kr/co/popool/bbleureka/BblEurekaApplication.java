89438f4363725f2419ae54b654fc398678c953d3
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2022-07-20 01:41:59 +0900

    Feat: EnableEurekaServer 어노테이션 추가 #67

--- bbl-eureka/src/main/java/kr/co/popool/bbleureka/BblEurekaApplication.java
+++ bbl-eureka/src/main/java/kr/co/popool/bbleureka/BblEurekaApplication.java
@@ -2,7 +2,9 @@ package kr.co.popool.bbleureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class BblEurekaApplication {
 
