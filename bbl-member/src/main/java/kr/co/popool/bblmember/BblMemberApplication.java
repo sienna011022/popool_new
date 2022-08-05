e0b5ffa051791dd093c558be7fac910a6165fd22
홍혁준 <jjj8337721@naver.com>
2022-08-05 16:57:33 +0900

    feature/118

--- bbl-member/src/main/java/kr/co/popool/bblmember/BblMemberApplication.java
+++ bbl-member/src/main/java/kr/co/popool/bblmember/BblMemberApplication.java
@@ -2,8 +2,10 @@ package kr.co.popool.bblmember;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BblMemberApplication {
 
     public static void main(String[] args) {
