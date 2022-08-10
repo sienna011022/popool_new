7bd2f4f53b189c07925c085fb1abc10302d4274f
sienna <20200803@sungshin.ac.kr>
2022-08-10 22:46:20 +0900

    eureka client 설정#126

--- bbl-career/src/main/java/kr/co/popool/BblCareerApplication.java
+++ bbl-career/src/main/java/kr/co/popool/BblCareerApplication.java
@@ -2,6 +2,7 @@ package kr.co.popool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @EnableFeignClients
 @EnableEurekaClient
+
 public class BblCareerApplication {
 
 	public static void main(String[] args) {
