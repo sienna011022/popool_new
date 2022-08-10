3daa99f8f8cf74f2790943cea3cf7659663ade8a
sienna <20200803@sungshin.ac.kr>
2022-08-10 23:04:10 +0900

    feat:DB변경#128

--- bbl-career/src/main/java/kr/co/popool/BblCareerApplication.java
+++ bbl-career/src/main/java/kr/co/popool/BblCareerApplication.java
@@ -2,15 +2,13 @@ package kr.co.popool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
-@EnableEurekaClient
+
 
 public class BblCareerApplication {
 
