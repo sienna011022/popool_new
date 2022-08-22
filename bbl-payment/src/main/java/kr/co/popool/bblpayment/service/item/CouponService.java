480a0e98a7ea49ff54b8ea0db0e1461901f00259
홍석주 <hsju0202@gmail.com>
2022-08-22 12:41:47 +0900

    feat: coupon CRUD

---
+++ bbl-payment/src/main/java/kr/co/popool/bblpayment/service/item/CouponService.java
@@ -0,0 +1,12 @@
+package kr.co.popool.bblpayment.service.item;
+
+
+import kr.co.popool.bblpayment.domain.dto.item.CouponDTO;
+
+public interface CouponService {
+
+    public void createCoupon(CouponDTO.CREATE create) throws Exception;
+    public void updateCoupon(CouponDTO.UPDATE update) throws Exception;
+    public CouponDTO.READ readCouponDetail(Long couponId) throws Exception;
+    public void deleteCoupon(Long couponId) throws Exception;
+}
