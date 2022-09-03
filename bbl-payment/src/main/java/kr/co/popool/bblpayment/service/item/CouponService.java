2a61bfc1e79120a6d977b1af0346bda16b1028e5
sochong <hsju0202@gmail.com>
2022-09-04 01:46:59 +0900

    feat: Coupon 도메인 수정

--- bbl-payment/src/main/java/kr/co/popool/bblpayment/service/item/CouponService.java
+++ bbl-payment/src/main/java/kr/co/popool/bblpayment/service/item/CouponService.java
@@ -7,6 +7,6 @@ public interface CouponService {
 
     public void createCoupon(CouponDTO.CREATE create) throws Exception;
     public void updateCoupon(CouponDTO.UPDATE update) throws Exception;
-    public CouponDTO.READ readCouponDetail(Long couponId) throws Exception;
+    public CouponDTO.DETAIL readCouponDetail(Long couponId) throws Exception;
     public void deleteCoupon(Long couponId) throws Exception;
 }
