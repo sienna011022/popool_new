e5196cabebd6efd4df7605f52b53602680f9a7be
홍혁준 <jjj8337721@naver.com>
2022-09-19 19:54:05 +0900

    feat : Payment 1. Interceptor 2. ThreadLocal 3. Handler Exception 4. Auditing 5. build.gradle update 6. yml eureka 등록

---
+++ bbl-payment/src/main/java/kr/co/popool/bblpayment/infra/handler/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package kr.co.popool.bblpayment.infra.handler;
+
+
+import kr.co.popool.bblcommon.error.exception.BusinessLogicException;
+import kr.co.popool.bblcommon.error.exception.UserDefineException;
+import kr.co.popool.bblcommon.error.model.ErrorResponse;
+import kr.co.popool.bblcommon.error.model.ResponseFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = {BusinessLogicException.class, RuntimeException.class})
+    public ResponseEntity handleRuntimeException(RuntimeException e){
+        ResponseFormat responseFormat = ResponseFormat.fail(e.getMessage());
+
+        return new ResponseEntity(responseFormat, HttpStatus.OK);
+    }
+
+    @ExceptionHandler(UserDefineException.class)
+    public ResponseEntity<ErrorResponse> handleUserDefineException(UserDefineException e) {
+        ResponseFormat responseFormat = ResponseFormat.fail(e.getMessage());
+
+        return new ResponseEntity(responseFormat, HttpStatus.OK);
+    }
+}
