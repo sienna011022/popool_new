1eef4f49a643390fad32cfd569f28db5f89fe80f
홍혁준 <jjj8337721@naver.com>
2022-09-19 20:08:31 +0900

    feat : Payment 1. Auditing Config update

---
+++ bbl-payment/src/main/java/kr/co/popool/bblpayment/infra/config/JpaAuditConfig.java
@@ -0,0 +1,17 @@
+package kr.co.popool.bblpayment.infra.config;
+
+
+import kr.co.popool.bblpayment.domain.shared.AuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditConfig {
+    @Bean
+    public AuditorAware<String> defaultAuditorAware(){
+        return new AuditorAwareImpl();
+    }
+}
